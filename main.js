1b8fed0018bc7fed88f57cd90f65e6352d6cf606
Branislav Carkic <branislavcharkic@gmail.com>
2021-08-13 21:36:07 +0200

    zadatak4

--- main.js
+++ main.js
@@ -22,34 +22,33 @@
 
 // zadatak 2
 
-// class Person {
-//     constructor(ime,age) {
-//         this.ime = ime;
-//         this.age = age;
-//     }
-//     compareAge = function(){
-
-//     }
-    
-// }
-
-// const iva = new Person('Iva', 8);
-// const sara = new Person('Sara',1);
 
 // zadatak3
-function vratiRec(recenica, rec){
-    let pozicija;
-    const arr = recenica.split(" ");
+// function vratiRec(recenica, rec){
+//     let pozicija;
+//     const arr = recenica.split(" ");
     
-    for (let i = 0; i < arr.length; i++) {
+//     for (let i = 0; i < arr.length; i++) {
         
-        if (arr[i] === rec) {    
-           pozicija = i;
-           return `rec "${rec}" se nalazi u recenici na poziciji ${pozicija}` 
-        } 
+//         if (arr[i] === rec) {    
+//            pozicija = i;
+//            return `rec "${rec}" se nalazi u recenici na poziciji ${pozicija}` 
+//         } 
         
-    }
+//     }
 
-    return `ne postoji rec ${rec} u recenici`;
+//     return `ne postoji rec ${rec} u recenici`;
+// }
+// console.log(vratiRec('Evo neka tamo recenica', 'tamo'));
+
+// zadatak4
+
+function transformisi2dU1dNiz(niz){
+  var newArr = [];
+
+  for (let i = 0; i < niz.length; i++) {
+     newArr = newArr.concat(niz[i]);   
+  }
+  console.log(newArr);
 }
-console.log(vratiRec('Evo neka tamo recenica', 'tamo'));
+transformisi2dU1dNiz([false, [1, 2], [3, 4], [5, 6], 7, 8, [9], [10], true]);
